24fc176ff8eb8c8a66ff78813e89c564efc03a1d
karoljanowski <karol.jj@icloud.com>
2023-03-03 14:50:39 +0100

    fix design

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -2,7 +2,13 @@
 /// Here we import all the JavaScript files we need for our homepage.
 
 import '../styles/home-page.scss'
+import '../styles/header.scss'
+import '../styles/cards.scss'
+import '../styles/black-card.scss'
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+
+import {
+    setupCounter
+} from '../../../global-scripts/scripts/counter/counter.js'
 
 setupCounter(document.querySelector('#counter'))
